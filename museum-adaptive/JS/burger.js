ba0c0c3efeda1ce6788d26ba48b483544e465b5f
Arxons <vasyrin1312@gmail.com>
2021-10-02 23:56:49 +0300

    feat: done 768 welcome visiting explore

--- museum-adaptive/JS/burger.js
+++ museum-adaptive/JS/burger.js
@@ -1,23 +1,19 @@
-const burgerOpen = document.getElementById('open');
-const burgerClose = document.getElementById('close');
-const burgerButton = document.getElementById('open')
-const burgerButtonOpen = document.querySelector('.open')
-const burgerButtonClose = document.querySelector('.close')
+const burgerButton = document.getElementById('open');
+const burgerButtonOpen = document.querySelector('.open');
+const titleWelcome = document.querySelector('.title-welcome');
 
 let counter = 1;
 
 function openClose() {
     if (counter === 1) {
-        burgerButton.style.width = '32px';
-        burgerButton.style.height = '25px';
         burgerButton.href = '#burger';
         burgerButtonOpen.className = 'close';
+        titleWelcome.className = 'title-welcome__hide'
         counter = 0;
     } else {
-        burgerButton.style.width = '32px';
-        burgerButton.style.height = '25px';
         burgerButton.href = '#';
         burgerButtonOpen.className = 'open';
+        titleWelcome.className = 'title-welcome'
         counter = 1;
     }
 }
