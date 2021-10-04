e875b2c8f363463bfb03cbafc14906bb63f94065
Arxons <vasyrin1312@gmail.com>
2021-10-04 22:06:38 +0300

    feat: change imgs formates

--- museum-adaptive/JS/gallery.js
+++ museum-adaptive/JS/gallery.js
@@ -5,8 +5,8 @@ const rightColumn = document.querySelector('.right-column');
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 nums.sort(() => Math.random() - 0.5);
 nums.forEach(elem => {
-    elem < 6 ? leftColumn.innerHTML += `<img src="images/gallery/galery${nums[elem - 1]}.jpg" alt="photo" width="455" height="auto">` :
-        elem >= 6 && elem < 11 ? middleColumn.innerHTML += `<img src="images/gallery/galery${nums[elem - 1]}.jpg" alt="photo" width="455" height="auto">` :
-            elem >= 11 ? rightColumn.innerHTML += `<img src="images/gallery/galery${nums[elem - 1]}.jpg" alt="photo" width="455" height="auto">` : elem;
+    elem < 6 ? leftColumn.innerHTML += `<img src="images/gallery/galery${nums[elem - 1]}.webp" alt="photo" width="455" height="auto">` :
+        elem >= 6 && elem < 11 ? middleColumn.innerHTML += `<img src="images/gallery/galery${nums[elem - 1]}.webp" alt="photo" width="455" height="auto">` :
+            elem >= 11 ? rightColumn.innerHTML += `<img src="images/gallery/galery${nums[elem - 1]}.webp" alt="photo" width="455" height="auto">` : elem;
 
 })
