2233d4a850902479cc897b6d3f2a73a5b37b759c
Arxons <vasyrin1312@gmail.com>
2021-11-15 22:17:41 +0300

    feat: done results

--- src/javascript/artists.js
+++ src/javascript/artists.js
@@ -1,60 +1,117 @@
 const artist = document.querySelector('.artist'),
     workArea = document.querySelector('.wrapper-work');
-import images from './images.js'
+import images from './images.js';
+
+let counterCorrect = 0;
+let counterAllAnsw = 1;
+let corRes = {
+    portrait: 0,
+    landscape: 0,
+    stillLife: 0,
+    graphic: 0,
+    antique: 0,
+    avantGarde: 0,
+    renaissance: 0,
+    surrealism: 0,
+    kitsch: 0,
+    minimalism: 0,
+    avangard: 0,
+    industrial: 0
+};
+let currentCategorie = [];
 
 function changeContent() {
     workArea.innerHTML = `<div class="categories">
         <h2>Choose categorie</h2>
         <button class="portrait" type="button">
+        <div class="image-with-res" id="portrait">
             <img src="./assets/img/0.jpg" alt="" width="150" height="150">
-            Portrait
+            <p class="res">${corRes.portrait}/9</p> 
+            </div>            
+            Portrait            
         </button>
         <button class="landscape" type="button">
+        <div class="image-with-res" id="landscape">
             <img src="./assets/img/10.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.landscape}/9</p> 
+            </div>
             Landscape
         </button>
         <button class="still-life" type="button">
+        <div class="image-with-res" id="still">
             <img src="./assets/img/20.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.stillLife}/9</p> 
+            </div>
             Still Life
         </button>
         <button class="graphic" type="button">
+        <div class="image-with-res" id="graphic">
             <img src="./assets/img/30.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.graphic}/9</p> 
+            </div>
             Graphic
         </button>
         <button class="antique" type="button">
+        <div class="image-with-res" id="antique">
             <img src="./assets/img/40.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.antique}/9</p> 
+            </div>
             Antique
         </button>
         <button class="avant-garde" type="button">
+        <div class="image-with-res" id="avant">
             <img src="./assets/img/50.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.avantGarde}/9</p> 
+            </div>
             Avant-Garde
         </button>
         <button class="renaissance" type="button">
+        <div class="image-with-res" id="renaissance">
             <img src="./assets/img/60.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.renaissance}/9</p> 
+            </div>
             Renaissance
         </button>
         <button class="surrealism" type="button">
+        <div class="image-with-res" id="surrealism">
             <img src="./assets/img/70.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.surrealism}/9</p> 
+            </div>
             Surrealism
         </button>
         <button class="kitsch" type="button">
+        <div class="image-with-res" id="kitsch">
             <img src="./assets/img/80.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.kitsch}/9</p> 
+            </div>
             Kitsch
         </button>
         <button class="minimalism" type="button">
+        <div class="image-with-res" id="minimalism">
             <img src="./assets/img/90.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.minimalism}/9</p> 
+            </div>
             Minimalism
         </button>
         <button class="avangard" type="button">
+        <div class="image-with-res" id="avangard">
             <img src="./assets/img/100.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.avangard}/9</p> 
+            </div>
             Avangard
         </button>
         <button class="industrial" type="button">
+        <div class="image-with-res" id="industrial">
             <img src="./assets/img/110.jpg" alt="" width="150" height="150">
+            <p class="res">${corRes.industrial}/9</p> 
+            </div>
             Industrial
         </button>
-    </div>`
-    chooseCategorie()
+    </div>`;
+    counterCorrect = 0;
+    counterAllAnsw = 1
+    chooseCategorie();
+    visCorRes()
 }
 
 artist.addEventListener('click', changeContent)
@@ -67,22 +124,24 @@ function chooseCategorie() {
                 workArea.innerHTML = `<h2 class="question">Кто автор данной картины?</h2>
                 <img class="quest-img" src="./assets/img/${0}.jpg" alt="" width="150" height="150">
                 <ul class="answers">
-                    ${correctAnswer(i)}
+                    ${getAnswers(i)}
                 </ul>
-                <div class="counter">
-                    <div class="all-answers">1/10</div>
+                <div class="counter">                    
                     <div class="correct-answers">Correct answers: 0</div>
-                </div>`
+                    <div class="all-answers">1/9</div>
+                </div>`;
+                correctAnswer(i + 1)
             } else {
                 workArea.innerHTML = `<h2 class="question">Кто автор данной картины?</h2>
                 <img class="quest-img" src="./assets/img/${i}0.jpg" alt="" width="150" height="150">
                 <ul class="answers">
-                    ${correctAnswer(i * 10)}
+                    ${getAnswers(i * 10)}
                 </ul>
-                <div class="counter">
-                    <div class="all-answers">1/10</div>
+                <div class="counter">                    
                     <div class="correct-answers">Correct answers: 0</div>
-                </div>`
+                    <div class="all-answers">1/9</div>
+                </div>`;
+                correctAnswer(i * 10)
             }
         })
     })
@@ -92,30 +151,114 @@ function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function correctAnswer(i) {
+function getAnswers(i) {
     let answers = null;
     const random = getRandom(1, 4)
     if (random === 1) {
         answers = `<li class="correct">${images[i].author}</li>
-        <li>${images[i + 1].author}</li>
-        <li>${images[i + 2].author}</li>
-        <li>${images[i + 3].author}</li>`
+        <li class="incorrect">${images[i + 1].author}</li>
+        <li class="incorrect">${images[i + 2].author}</li>
+        <li class="incorrect">${images[i + 3].author}</li>`
     } else if (random === 2) {
-        answers = `<li>${images[i + 1].author}</li>
+        answers = `<li class="incorrect">${images[i + 1].author}</li>
         <li class="correct">${images[i].author}</li>
-        <li>${images[i + 2].author}</li>
-        <li>${images[i + 3].author}</li>`
+        <li class="incorrect">${images[i + 2].author}</li>
+        <li class="incorrect">${images[i + 3].author}</li>`
     } else if (random === 3) {
-        answers = `<li>${images[i + 1].author}</li>
-        <li>${images[i + 2].author}</li>
+        answers = `<li class="incorrect">${images[i + 1].author}</li>
+        <li class="incorrect">${images[i + 2].author}</li>
         <li class="correct">${images[i].author}</li>
-        <li>${images[i + 3].author}</li>`
+        <li class="incorrect">${images[i + 3].author}</li>`
     } else if (random === 4) {
-        answers = `<li>${images[i + 1].author}</li>
-        <li>${images[i + 2].author}</li>
-        <li>${images[i + 3].author}</li>
+        answers = `<li class="incorrect">${images[i + 1].author}</li>
+        <li class="incorrect">${images[i + 2].author}</li>
+        <li class="incorrect">${images[i + 3].author}</li>
         <li class="correct">${images[i].author}</li>`
     }
     return answers;
 }
 
+
+
+function correctAnswer(index) {
+    const corAnsw = document.querySelector('.correct');
+    const incorAnsw = document.querySelectorAll('.incorrect');
+    let lowerIndex = 0;
+    if (index >= 10) lowerIndex = index % 10;
+    corAnsw.addEventListener('click', () => {
+        counterCorrect++
+        counterAllAnsw++
+        workArea.innerHTML = `<h2 class="question">Кто автор данной картины?</h2>
+        <img class="quest-img" src="./assets/img/${index + 1}.jpg" alt="" width="150" height="150">
+        <ul class="answers">
+            ${getAnswers(index + 1)}
+        </ul>
+        <div class="counter">            
+    <div class="correct-answers">Correct answers: ${counterCorrect}</div>
+    <div class="all-answers">${counterAllAnsw}/9</div >
+        </div > `
+        correctAnswer(index + 1)
+    })
+    incorAnsw.forEach((el, i) => {
+        incorAnsw[i].addEventListener('click', () => {
+            const main = document.querySelector('.main-wrapper')
+            incorAnsw[i].style.backgroundColor = 'darkred';
+            main.style.transform = 'translate(-5%)'
+            setTimeout(() => main.style.transform = 'translate(5%)', 100)
+            setTimeout(() => main.style.transform = 'translate(0)', 200)
+            setTimeout(() => {
+                counterAllAnsw++
+                workArea.innerHTML = `<h2 class="question">Кто автор данной картины?</h2>
+        <img class="quest-img" src="./assets/img/${index + 1}.jpg" alt="" width="150" height="150">
+        <ul class="answers">
+            ${getAnswers(index + 1)}
+        </ul>
+        <div class="counter">            
+    <div class="correct-answers">Correct answers: ${counterCorrect}</div>
+    <div class="all-answers">${counterAllAnsw}/9</div >
+        </div > `
+                correctAnswer(index + 1)
+            }, 400)
+        })
+    })
+    if (counterAllAnsw > 9) {
+        currentCategorie.push(index - 9);
+        console.log(currentCategorie)
+        switch (visCorRes()) {
+            case 'portrait': corRes.portrait = counterCorrect; break;
+            case 'landscape': corRes.landscape = counterCorrect; break;
+            case 'stillLife': corRes.stillLife = counterCorrect; break;
+            case 'graphic': corRes.graphic = counterCorrect; break;
+            case 'antique': corRes.antique = counterCorrect; break;
+            case 'avantGarde': corRes.avantGarde = counterCorrect; break;
+            case 'renaissance': corRes.renaissance = counterCorrect; break;
+            case 'surrealism': corRes.surrealism = counterCorrect; break;
+            case 'kitsch': corRes.kitsch = counterCorrect; break;
+            case 'minimalism': corRes.minimalism = counterCorrect; break;
+            case 'avangard': corRes.avangard = counterCorrect; break;
+            case 'industrial': corRes.industrial = counterCorrect; break;
+        }
+        corAnsw.addEventListener('click', changeContent())
+        incorAnsw.forEach((el, i) => {
+            incorAnsw[i].addEventListener('click', changeContent())
+        })
+        currentCategorie = [];
+    }
+}
+
+function visCorRes() {
+    switch (currentCategorie[0]) {
+        case 1: return 'portrait';
+        case 10: return 'landscape';
+        case 20: return 'stillLife';
+        case 30: return 'graphic';
+        case 40: return 'antique';
+        case 50: return 'avantGarde';
+        case 60: return 'renaissance';
+        case 70: return 'surrealism';
+        case 80: return 'kitsch';
+        case 90: return 'minimalism';
+        case 100: return 'avangard';
+        case 110: return 'industrial';
+    }
+}
