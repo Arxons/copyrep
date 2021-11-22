e93f7bb9cfe108a111e21fa0400e5420677eade2
Arxons <vasyrin1312@gmail.com>
2021-11-22 21:23:03 +0300

    feat: run product mode

--- README.md
+++
@@ -1,4 +0,0 @@
-# rsschool-cv
-Private repository for @arxons
-
-https://rolling-scopes-school.github.io/arxons-JSFE2021Q3/museum
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './css/reset.css'
-import './css/main.css'
-import './javascript/artists.js'
-// import './javascript/pictures.js'
-
-console.log('Не сделаны настройки и отображение картинок после загрузки. -40 и -10 соответственно. Общий балл 170')
